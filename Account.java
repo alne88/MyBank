c37574bc0600346de7789b680e9e40b6d2c6abea
Alo <41338019+alne88@users.noreply.github.com>
2018-08-07 18:32:56 +0300

    Day 1

--- Account.java
+++ Account.java
@@ -1,4 +1,62 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
 public class Account {
+
+    private double balance;
+    private long accountNumber;
+    private String currency;
+    User owner;
+
+    public Account(String currency) {
+        this.balance = 0;
+        this.accountNumber = 10000000 + (long) (Math.random() * 10000000L);
+        this.currency = currency;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public void addMoney (double amountOfMoney) {
+        this.balance += amountOfMoney;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "balance=" + balance +
+                ", accountNumber=" + accountNumber +
+                ", currency='" + currency + '\'' +
+                ", owner=" + owner +
+                '}';
+    }
 }
--- Bank.java
+++ Bank.java
@@ -1,2 +1,45 @@
-public class Client {
+import java.util.List;
+
+public class Bank {
+
+    List<User> users;
+    String bankName;
+
+    public Bank(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public void addNewUser(String password,
+                           String fullName,
+                           String aadress,
+                           String phoneNumber,
+                           String email,
+                           String secretQuestion,
+                           String answerForSecretQuestion) {
+
+        User newUser = new User(password, fullName, aadress, phoneNumber, email, secretQuestion, answerForSecretQuestion);
+        users.add(newUser);
+    }
+
+    public void addUser (User user) {           //user already exist, we want to move the user to our bank (for ex from another branch)
+        users.add(user);
+    }
+
+    public Double totalAmountOfMoney (String userId) {
+        for (User user: users) {
+            if (userId.equals(userId)){
+                return user.totalAmountOfMoney();
+            }
+        }
+        return null;
+    }
+
+    public void addMoneyToUser (String userId, long accountId, double amountOfMoney) {
+        for (User user: users) {
+            if (userId.equals(user.getId())){
+                user.addMoneyToAccount(accountId, amountOfMoney);
+            }
+        }
+    }
+
 }
